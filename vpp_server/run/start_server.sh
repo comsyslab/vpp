1df270e55582234dd620f8033072a99560b39fb4
Ubbe Welling <ubbe@eng.au.dk>
2015-12-08 10:22:15 +0100

    Fixed start script

--- vpp_server/run/start_server.sh
+++ vpp_server/run/start_server.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-export PYTHONPATH=${PYTHONPATH}:../src
+export PYTHONPATH=$PYTHONPATH:../src
 echo Redirecting output to ../logs/console.log
 python ../src/vpp/core/coordinator.py >> ../logs/console.log 2>&1 &
 sleep 1
