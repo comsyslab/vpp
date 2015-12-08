f5882679e1e5d3e95783ec71cbf90641ef0fbfe3
Ubbe Welling <ubbe@eng.au.dk>
2015-12-08 10:43:58 +0100

    Fixed relative path in start script

--- vpp_server/run/start_server.sh
+++ vpp_server/run/start_server.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
-export PYTHONPATH=$PYTHONPATH:../src
+export PYTHONPATH=$PYTHONPATH:$PWD/../src
+echo $PYTHONPATH
 echo Redirecting output to ../logs/console.log
 python ../src/vpp/core/coordinator.py >> ../logs/console.log 2>&1 &
+
+
 sleep 1
