358dd278efea2703dd18cf2f33e75ffb827414f1
ubbewelling <ubbe@eng.au.dk>
2016-04-13 15:09:33 +0200

    Increased DB connection pool size to 20

--- vpp_server/src/vpp/database/sql_alch_util.py
+++ vpp_server/src/vpp/database/sql_alch_util.py
@@ -2,7 +2,7 @@ import sqlalchemy
 
 
 def create_engine_and_session(db_string, autoflush=True):
-    engine = sqlalchemy.create_engine(db_string, echo=False)
+    engine = sqlalchemy.create_engine(db_string, pool_size=20, echo=False)
     SessionCls = sqlalchemy.orm.sessionmaker(bind=engine)
     session = SessionCls()
     session.autoflush = autoflush
