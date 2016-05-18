620058d7dbc43c3237fafe6df7f69091d3e235b5
ubbewelling <ubbe@eng.au.dk>
2016-05-18 14:53:25 +0200

    DBMaintenance now supports transferring measurements and predictions to datawarehouse, doing hard-coded averaging of values in the process. Configuration of averaging pending

--- vpp_server/src/vpp/database/db_maintenance.py
+++ vpp_server/src/vpp/database/db_maintenance.py
@@ -45,11 +45,12 @@ class DBMaintenance(object):
         self._transfer_main_tables_to_dw()
 
         #find subtables scheduled for transfer and deletion - and search back for any old tables
+        self._create_round_func()
         for part_back_index in range(0, 150):
             secs_to_look_back = self.window_seconds + part_back_index * self.partitition_period_seconds
             partition_timestamp = datetime.datetime.now(tz=tzlocal.get_localzone()) - datetime.timedelta(seconds=secs_to_look_back)
             self._transfer_subtables_to_dw(partition_timestamp)
-            self._drop_for_timestamp(partition_timestamp)
+            #self._drop_for_timestamp(partition_timestamp)
 
         self.db_manager_local.close()
         self.db_manager_dw.close()
@@ -73,18 +74,19 @@ class DBMaintenance(object):
         meas_table_name = self.db_manager_local.schema_manager.get_measurement_subtable_name(partition_timestamp)
         pred_table_name = self.db_manager_local.schema_manager.get_prediction_subtable_name(partition_timestamp)
 
+        averaging_interval_secs = None #60*60 * 1 #1 hour
         if self._table_exists_locally(meas_table_name):
             self.db_manager_dw.schema_manager.get_or_create_measurement_subtable(partition_timestamp)
-            self._copy_meas_table_contents_sql(meas_table_name)
+            self._copy_meas_table(meas_table_name, averaging_interval_secs)
 
         if self._table_exists_locally(pred_table_name):
             self.db_manager_dw.schema_manager.get_or_create_prediction_subtable(partition_timestamp)
-            self._copy_pred_table_contents_sql(pred_table_name)
+            self._copy_pred_table(pred_table_name, averaging_interval_secs)
 
         self.db_manager_dw.commit()
 
     def _drop_for_timestamp(self, timestamp):
-        self.logger.info("DBMaintenance dropping subtables for time " + str(timestamp))
+        self.logger.debug("DBMaintenance dropping subtables for time " + str(timestamp))
         self.db_manager_local.commit()  # Necessary to prevent DROP TABLE from blocking
 
         meas_table_name = self.db_manager_local.schema_manager.get_measurement_subtable_name(timestamp)
@@ -120,52 +122,67 @@ class DBMaintenance(object):
             string = 'Table %s transferred total of %d entries to DW in %.2f secs, average %.1f ms/entry' % (table_local.name, count, time_spent, avg_time)
             self.logger.info(string)
 
-    def _copy_meas_table_contents_sql(self, table_name):
+    def _copy_meas_table(self, table_name, averaging_interval_secs = None):
         columns = 'sensor_id, timestamp , value'
         columns_w_types = 'sensor_id character varying, timestamp timestamp with time zone, value character varying'
-        self._copy_table_contents_sql(table_name, columns, columns_w_types)
 
-    def _copy_pred_table_contents_sql(self, table_name):
+        query_sql = self._get_simple_query_sql(table_name, columns)
+        if averaging_interval_secs is not None:
+            query_sql = self._get_meas_averaging_sql(table_name, averaging_interval_secs)
+
+        self._copy_table_custom_sql(table_name, columns, columns_w_types, query_sql)
+
+    def _copy_pred_table(self, table_name, averaging_interval_secs = None):
         columns = 'endpoint_id, timestamp, value, time_received, value_interval'
         columns_w_types = 'endpoint_id character varying, timestamp timestamp with time zone, value character varying, time_received timestamp with time zone, value_interval interval'
-        self._copy_table_contents_sql(table_name, columns, columns_w_types)
 
-    def _copy_table_contents_sql(self, table_name, columns, columns_w_types):
-        '''sql_example = "CREATE EXTENSION IF NOT EXISTS dblink;"\
-              "SELECT dblink_exec('dbname=vpp port=5432 host=10.24.128.230 user=ubbe password=M1thmpw1', " \
-                                 "'CREATE EXTENSION IF NOT EXISTS dblink;"\
-                                  "INSERT INTO \"Measurement_2016_04_26_00_00_00\" (sensor_id, timestamp , value)"\
-                                      "SELECT *"\
-                                      "FROM dblink(''dbname=vpp port=5432 host=cx-14d94a.st.client.au.dk user=vpp password=comsysr00t'',"\
-                                                  "''SELECT sensor_id, timestamp , value FROM \"Measurement_2016_04_29_00_00_00\"'')"\
-                                      "AS meas(sensor_id character varying, timestamp timestamp with time zone, value character varying)"\
-              "');"'''
+        query_sql = self._get_simple_query_sql(table_name, columns)
+        if averaging_interval_secs is not None:
+            query_sql = self._get_pred_averaging_sql(table_name, averaging_interval_secs)
 
-        sql = "SELECT COUNT (*) FROM \""+ table_name +"\";"
-        result = self.db_manager_local.engine.execute(sql).first()
+        self._copy_table_custom_sql(table_name, columns, columns_w_types, query_sql)
+
+    def _get_meas_averaging_sql(self, table_name, averaging_interval):
+        return 'SELECT sensor_id, ' \
+                      'ts_round( "timestamp", ' + str(averaging_interval) + ' ) AS "timestamp", ' \
+                      'CAST(AVG(CAST(value AS float)) AS text) AS value ' \
+               'FROM "' + table_name + '" ' \
+               'GROUP BY 2, sensor_id ORDER BY sensor_id;'
+
+    def _get_pred_averaging_sql(self, table_name, averaging_interval_secs):
+        return 'SELECT endpoint_id, ' \
+                               'ts_round( "timestamp", ' + str(averaging_interval_secs) + ' ) AS "timestamp", ' \
+                               'CAST(AVG(CAST(value AS float)) AS text) AS value, ' \
+                               'MIN(time_received) AS time_received, ' \
+                               '\'\'' + str(averaging_interval_secs) + ' seconds\'\' AS value_interval ' \
+                        'FROM "' + table_name + '" ' \
+                        'GROUP BY 2, endpoint_id ORDER BY endpoint_id;'
+
+    def _get_simple_query_sql(self, table_name, columns):
+        return 'SELECT ' + columns + ' FROM "' + table_name + '";'
+
+    def _copy_table_custom_sql(self, table_name, columns, columns_w_types, query_sql):
+        count_sql = "SELECT COUNT (*) FROM \""+ table_name +"\";"
+        result = self.db_manager_local.engine.execute(count_sql).first()
         count = result[0]
 
-        sql = "CREATE EXTENSION IF NOT EXISTS dblink;"\
-              "SELECT dblink_exec('" + self.db_string_dw_dblink + "', " \
-                                  "'CREATE EXTENSION IF NOT EXISTS dblink; "\
-                                   "INSERT INTO \"" + table_name + "\" (" + columns + ") "\
-                                       "SELECT * "\
-                                       "FROM dblink(''" + self.db_string_local_dblink + "'',"\
-                                                    "''SELECT " + columns + " FROM \"" + table_name + "\"'') "\
-                                       "AS meas(" + columns_w_types + ") "\
-              "');"
+        sql = "CREATE EXTENSION IF NOT EXISTS dblink; "\
+               "INSERT INTO \"" + table_name + "\" (" + columns + ") "\
+                   "SELECT * "\
+                   "FROM dblink('" + self.db_string_local_dblink + "',"\
+                                "'" + query_sql + "') "\
+                   "AS temp_table(" + columns_w_types + ");"
 
         self.logger.debug("Executing " + sql)
         try:
             time_start = time.time()
-            result = self.db_manager_local.engine.execute(sql)
-            time_spent = (time.time() - time_start) / 1000
-            avg_time = time_spent / count
-            string = 'Table %s transferred total of %d entries to DW in %.2f secs, average %.1f ms/entry' % (table_name, count, time_spent, avg_time)
+            result = self.db_manager_dw.engine.execute(sql)
+            time_spent = (time.time() - time_start)
+            avg_time = (time_spent * 1000) / count
+            string = 'Table %s processed %d entries and inserted %d rows in DW in %.2f secs, average %.3f ms/entry' % (table_name, count, result.rowcount, time_spent, avg_time)
             self.logger.info(string)
         except Exception as e:
-            self.logger.exception("Exception while executing SQL: " + sql + ".\n Exception says: " + str(e.message))
-
+            self.logger.exception("Exception while executing SQL: " + sql + "\n Exception says: " + str(e.message))
 
     def _quick_mapper(self, table):
         Base = declarative_base()
@@ -175,6 +192,16 @@ class DBMaintenance(object):
 
     def _table_exists_locally(self, table_name):
         return self.db_manager_local.schema_manager.lookup_table(table_name) != None
+
+    def _create_round_func(self):
+        sql = "CREATE OR REPLACE FUNCTION ts_round( timestamptz, INT4 ) RETURNS TIMESTAMPTZ AS $$"\
+              "SELECT 'epoch'::timestamptz + '1 second'::INTERVAL * ( $2 * ( extract( epoch FROM $1 )::INT4 / $2 ) );"\
+               "$$ LANGUAGE SQL;"
+
+        try:
+            result = self.db_manager_local.engine.execute(sql)
+        except Exception as e:
+            self.logger.exception("Exception while executing SQL: " + sql + ".\n Exception says: " + str(e.message))
 
 if __name__ == '__main__':
     pass
